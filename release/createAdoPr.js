0b4bbb52aec39755102101e6c488eaf1e29f1b06
maroghel <maroghel@microsoft.com>
2020-06-04 10:15:46 -0400

    ls integrations

--- release/createAdoPr.js
+++ release/createAdoPr.js
@@ -69,6 +69,7 @@ async function commitADOL2Changes(directory, release)
     }
 
     util.execInForeground(`cat ${file}`, directory, opt.dryrun);
+    util.execInForeground(`ls ${INTEGRATION_DIR}`, directory, opt.dryrun);
     util.execInForeground(`ls`, directory, opt.dryrun);
     util.execInForeground(`ls ${directory}`, directory, opt.dryrun);
     util.execInForeground(`ls DistributedTask`, directory, opt.dryrun);
